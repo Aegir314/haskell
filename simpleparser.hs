4567efcf9db30e2173c5ed76a80bd608fedb9c31
Aegir314 <aegir@archlinux.info>
2014-11-26 11:18:40 +0100

    Scheme interpreter

--- simpleparser.hs
+++ simpleparser.hs
@@ -2,6 +2,7 @@ module Main where
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 import Control.Monad
+import Control.Monad.Error
 
 main :: IO ()
 main = getArgs >>= putStrLn . show . eval . readExpr . (!! 0) 
@@ -101,4 +102,29 @@ eval val@(Number _) = val
 eval val@(Bool _) = val
 eval (List [Atom "quote", val]) = val
 eval (List (Atom func : args)) = apply func $ map eval args
+
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+--lookup searcher by keyi, primitives is list of pairs of string and functions
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives =  [( "+", numericBinop (+)),
+               ( "-", numericBinop (-)),
+               ( "*", numericBinop (*)),
+               ( "/", numericBinop div),
+               ( "mod", numericBinop mod),
+               ( "quotient", numericBinop quot),
+               ( "remainder", numericBinop rem)]
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+--reads returns list of pairs of (in our case:)(parsed value, original value)
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = let parsed = reads n in
+                            if null parsed
+                                then 0
+                                else fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
+
 
