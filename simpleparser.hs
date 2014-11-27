51ab8cc596788d563e7cae2d36adcca9562b8711
Aegir314 <aegir@archlinux.info>
2014-11-27 14:32:25 +0100

    added if

--- simpleparser.hs
+++ simpleparser.hs
@@ -1,74 +1,51 @@
 module Main where
-import System.Environment
-import Text.ParserCombinators.Parsec hiding (spaces)
+
 import Control.Monad
+import System.Environment
 import Control.Monad.Error
+import Text.ParserCombinators.Parsec hiding (spaces)
 
 main :: IO ()
 main = do
-         args <- getArgs
-         evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
-         putStrLn $ extractValue $ trapError evaled
+    args <- getArgs
+    evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
+    putStrLn $ extractValue $ trapError evaled
 
 symbol :: Parser Char
-symbol = oneOf "$!%&|*+-/:<=?>@^_^#"
-
-spaces :: Parser ()
-spaces = skipMany1 space
-
---monadic bind >> does left thing first then right, whole expression fails
---if one of those fails
+symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
 
 readExpr :: String -> ThrowsError LispVal
 readExpr input = case parse parseExpr "lisp" input of
     Left err -> throwError $ Parser err
     Right val -> return val
 
+spaces :: Parser ()
+spaces = skipMany1 space
+
 data LispVal = Atom String
-             | List [LispVal] 
-             | DottedList [LispVal] LispVal --improper list stores list of all elements but the last and then stores the last as another field
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
              | Number Integer
              | String String
              | Bool Bool
 
 parseString :: Parser LispVal
-parseString = do 
-                char '"'
-                x <- many (noneOf "\"")
-                char '"'
-                return $ String x --return lifts String x into parser nomad so whole action has type Parser LispVal
+parseString = do char '"'
+                 x <- many (noneOf "\"")
+                 char '"'
+                 return $ String x
 
-
---that new operator  tries first parser if fails tries second
 parseAtom :: Parser LispVal
-parseAtom = do
-              first <- letter <|> symbol
-              rest <- many (letter <|> digit <|> symbol)
-              let atom = [first] ++ rest
-              return $ case atom of
-                        "#t" -> Bool True
-                        "#f" -> Bool False
-                        otherwise -> Atom atom
+parseAtom = do first <- letter <|> symbol
+               rest <- many (letter <|> digit <|> symbol)
+               let atom = [first] ++ rest
+               return $ case atom of 
+                          "#t" -> Bool True
+                          "#f" -> Bool False
+                          otherwise -> Atom atom
 
---many1 matches 1 or more digit
---read converts that string to a number and then we pass that to a Number to
---get ListVal but many1 digit return Parser String so we use liftM to tell
---(Number . read) to just operate on a value inside Monad
 parseNumber :: Parser LispVal
 parseNumber = liftM (Number . read) $ many1 digit
---try combinator attempts to run specified parser but if it fails it backs up
---to the previous state. Allows using alternative without interfering with the
---other alternative
-parseExpr :: Parser LispVal
-parseExpr = parseAtom
-        <|> parseNumber
-        <|> parseString
-        <|> parseQuoted
-        <|> do 
-              char '('
-              x<-(try parseList) <|> parseDottedList
-              char ')'
-              return x
 
 parseList :: Parser LispVal
 parseList = liftM List $ sepBy parseExpr spaces
@@ -81,56 +58,106 @@ parseDottedList = do
 
 parseQuoted :: Parser LispVal
 parseQuoted = do
-                char '\''
-                x<-parseExpr
-                return $ List [Atom "quote", x]
+    char '\''
+    x <- parseExpr
+    return $ List [Atom "quote", x]
 
-instance Show LispVal where show = showVal
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
+        <|> parseQuoted
+        <|> do char '('
+               x <- (try parseList) <|> parseDottedList
+               char ')'
+               return x
 
 showVal :: LispVal -> String
 showVal (String contents) = "\"" ++ contents ++ "\""
 showVal (Atom name) = name
 showVal (Number contents) = show contents
 showVal (Bool True) = "#t"
-showVal (Bool false) = "#f"
+showVal (Bool False) = "#f"
 showVal (List contents) = "(" ++ unwordsList contents ++ ")"
-showVal (DottedList head tail) = "(" ++ unwordsList head ++ "." ++ showVal tail ++ ")"
+showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
 
 unwordsList :: [LispVal] -> String
 unwordsList = unwords . map showVal
 
+instance Show LispVal where show = showVal
+
 eval :: LispVal -> ThrowsError LispVal
-eval val@(String _ ) = return val
+eval val@(String _) = return val
 eval val@(Number _) = return val
 eval val@(Bool _) = return val
 eval (List [Atom "quote", val]) = return val
+eval (List [Atom "if", pred, conseq, alt]) = 
+    do result <- eval pred
+       case result of
+         Bool False -> eval alt
+         otherwise -> eval conseq
 eval (List (Atom func : args)) = mapM eval args >>= apply func
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
 
-apply :: String -> [LispVal] -> ThrowsError LispVal 
-apply func args = maybe ( throwError $ NotFunction "Unrecognized primitive function args" func) ($args) (lookup func primitives)
---lookup searcher by keyi, primitives is list of pairs of string and functions
+apply :: String -> [LispVal] -> ThrowsError LispVal
+apply func args = maybe (throwError $ NotFunction "Unrecognized primitive function args" func)
+                        ($ args)
+                        (lookup func primitives)
+
 primitives :: [(String, [LispVal] -> ThrowsError LispVal)]
-primitives =  [( "+", numericBinop (+)),
-               ( "-", numericBinop (-)),
-               ( "*", numericBinop (*)),
-               ( "/", numericBinop div),
-               ( "mod", numericBinop mod),
-               ( "quotient", numericBinop quot),
-               ( "remainder", numericBinop rem)]
+primitives = [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem),
+              ("=", numBoolBinop (==)),
+              ("<", numBoolBinop (<)),
+              (">", numBoolBinop (>)),
+              ("/=", numBoolBinop (/=)),
+              (">=", numBoolBinop (>=)),
+              ("<=", numBoolBinop (<=)),
+              ("&&", boolBoolBinop (&&)),
+              ("||", boolBoolBinop (||)),
+              ("string=?", strBoolBinop (==)),
+              ("string?", strBoolBinop (>)),
+              ("string<=?", strBoolBinop (<=)),
+              ("string>=?", strBoolBinop (>=))]
 
 numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
 numericBinop op singleVal@[_] = throwError $ NumArgs 2 singleVal
 numericBinop op params = mapM unpackNum params >>= return . Number . foldl1 op
---reads returns list of pairs of (in our case:)(parsed value, original value)
+
+boolBinop :: (LispVal -> ThrowsError a) -> (a -> a -> Bool) -> [LispVal] -> ThrowsError LispVal
+boolBinop unpacker op args = if length args /= 2 
+                             then throwError $ NumArgs 2 args
+                             else do left <- unpacker $ args !! 0
+                                     right <- unpacker $ args !! 1
+                                     return $ Bool $ left `op` right
+
+numBoolBinop = boolBinop unpackNum
+strBoolBinop = boolBinop unpackStr
+boolBoolBinop = boolBinop unpackBool
+
 unpackNum :: LispVal -> ThrowsError Integer
 unpackNum (Number n) = return n
-unpackNum (String n) = let parsed = reads n in
-                            if null parsed
-                                then throwError $ TypeMismatch "number" $ String n
-                                else return $ fst $ parsed !! 0
+unpackNum (String n) = let parsed = reads n in 
+                          if null parsed 
+                            then throwError $ TypeMismatch "number" $ String n
+                            else return $ fst $ parsed !! 0
 unpackNum (List [n]) = unpackNum n
 unpackNum notNum = throwError $ TypeMismatch "number" notNum
+
+unpackStr :: LispVal -> ThrowsError String
+unpackStr (String s) = return s
+unpackStr (Number s) = return $ show s
+unpackStr (Bool s) = return $ show s
+unpackStr notString = throwError $ TypeMismatch "string" notString
+
+unpackBool :: LispVal -> ThrowsError Bool
+unpackBool (Bool b) = return b
+unpackBool notBool = throwError $ TypeMismatch "boolean" notBool
 
 
 data LispError = NumArgs Integer [LispVal]
@@ -142,32 +169,24 @@ data LispError = NumArgs Integer [LispVal]
                | Default String
 
 showError :: LispError -> String
-
 showError (UnboundVar message varname) = message ++ ": " ++ varname
-
 showError (BadSpecialForm message form) = message ++ ": " ++ show form
-
 showError (NotFunction message func) = message ++ ": " ++ show func
-
 showError (NumArgs expected found) = "Expected " ++ show expected 
-                                  ++ " args: found values " ++ 
-                                     unwordsList found 
-showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected ++ ", found " ++ show found
-
+                                  ++ " args: found values " ++ unwordsList found
+showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected
+                                       ++ ", found " ++ show found
 showError (Parser parseErr) = "Parse error at " ++ show parseErr
 
 instance Show LispError where show = showError
 
 instance Error LispError where
-    noMsg = Default "An error has occured"
-    strMsg = Default
+     noMsg = Default "An error has occurred"
+     strMsg = Default
 
 type ThrowsError = Either LispError
 
 trapError action = catchError action (return . show)
 
---error goes to left so we leave it undefined 
 extractValue :: ThrowsError a -> a
 extractValue (Right val) = val
-
-
